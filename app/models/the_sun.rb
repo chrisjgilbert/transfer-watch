78f68b6df396a114d18cde4671ec23db04c3a548
Chris Gilbert <chrisgilbert4@icloud.com>
2020-06-26 15:42:10 +0100

    WIP: add sun scraper

--- app/models/the_sun.rb
+++ app/models/the_sun.rb
@@ -1,29 +1,33 @@
 require 'httparty'
 require 'nokogiri'
-require "google/cloud/language"
+require 'google/cloud/language'
 
-URL = 'https://www.bbc.co.uk/sport/football/gossip'
-
-namespace :scrape do
-  desc "The Sun"
-
-  task :the_sun => :environment do
-    TheSun.new.scrape
+class TheSun
+  def initialize
+    @url = 'https://www.thesun.co.uk/sport/football/transfer-news/'
   end
 
-  desc "Scrape the BBC Sport Gossip Column for Rumours"
-  task :bbc => :environment do
-    language = Google::Cloud::Language.language_service
-    page = Nokogiri::HTML(HTTParty.get(URL))
-    page.css('div#story-body p').each do |story|
+  def language
+    Google::Cloud::Language.language_service
+  end
 
-      if story.text.match(/transfer/i) || story.text.match(/signing/i) || story.text.match(/move/i)
+  def sanitize_text(story)
+    story.text.gsub(/[^0-9A-Za-z.\-]/, ' ')
+  end
+  
+  def scrape
+    page = Nokogiri::HTML(HTTParty.get(@url))
+
+    page.css('#main-content a').each do |story|
+      next if story.blank?
+      story = sanitize_text(story)
+      if story.match(/transfer/i) || story.match(/signing/i) || story.match(/move/i) || story.match(/signing/i)
 
         # call Google's API
-        document = { content: story.text, type: :PLAIN_TEXT }
+        document = { content: story, type: :PLAIN_TEXT }
         response = language.analyze_entities document: document
         entities = response.entities
-        
+
         # Extract the player and clubs from Google response
         players = []
         clubs = []
@@ -34,7 +38,7 @@ namespace :scrape do
               players << player
             end
           end
-          
+
           # matching Based on Wikiepdia URL seems more reliable than entity.type == :ORGANIZATION
           # and then looking up the name in our DB
           entity_url = entity.metadata['wikipedia_url']
@@ -43,7 +47,9 @@ namespace :scrape do
             clubs << club
           end   
         end
-
+        
+        p players
+        p clubs
         # this means we will ignore stories with
         # multiple players (for now)
         # and also skip if we don't find any
@@ -54,9 +60,9 @@ namespace :scrape do
         matched_player = players.first
         clubs.each do |linked_club|
           next if matched_player.plays_for_club?(linked_club) || matched_player.already_linked_with_club?(linked_club)
-          rumour = Rumour.new(description: story.text, player_id: matched_player.id, club_id: linked_club.id)
+          rumour = Rumour.new(description: story, player_id: matched_player.id, club_id: linked_club.id)
           p "*"*100
-          p "From the text: #{story.text}"
+          p "From the text: #{story}"
           if rumour.save 
             p "Successfully created a rumour linking #{matched_player.name} with #{linked_club.name}!"
           else
@@ -68,8 +74,4 @@ namespace :scrape do
     end
   end
 end
-
-
-
-
 
